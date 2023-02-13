6db169fea8af9a026fa3360d7052e2f04050586c
Dan159159 <ju.azurduy@aulanz.net>
2023-02-13 09:44:11 +0100

    Poniendolo bonito

---
+++ FOL/FOL.md
@@ -0,0 +1,55 @@
+## Organigrama
+
+    CEO (Chief Executive Officer)
+
+            CMO (Chief Marketing Officer)
+
+            CFO (Chief Financial Officer)
+   
+            Gerente de Flota
+
+            
+
+
+
+
+
+| Cargo                            | Nombre   |
+| ------------------------------ | -------- |
+| CEO                             | Xabi     |
+| CMO                             | Jennyfer |
+| CFO     |      Issam    |
+| Gerente de Operaciones          |     Daniel     |
+| Gerente de Flota al Cliente |      Issam    |
+
+
+## Descripcióin del organigrama
+
+### CEO (Chief Executive Officer)
+- Responsable de la estrategia general y la toma de decisiones importantes.
+
+### CFO (Chief Financial Officer)
+- Responsable de la gestión financiera de la compañía, incluyendo la contabilidad, la planificación presupuestaria y la toma de decisiones de inversión.
+
+### CMO (Chief Marketing Officer)
+- Responsable de la estrategia de marketing y la promoción de la marca de la empresa.
+
+### Gerente de Operaciones
+- Responsable de la ejecución efectiva de las operaciones diarias de la empresa, incluyendo la logística y la gestión de la flota de vehículos.
+
+### Gerente de Flota
+- Responsable de la gestión y mantenimiento de la flota de vehículos de la compañía.
+
+### Gerente de Servicio al Cliente
+- Responsable de garantizar una experiencia satisfactoria para los clientes y resolver cualquier problema que puedan tener.
+
+### Gerente de Ventas
+- Responsable de generar ingresos para la empresa a través de la identificación y adquisición de nuevos clientes.
+
+### Gerente de Desarrollo de Negocios
+- Responsable de identificar y explotar oportunidades de crecimiento para la empresa.
+
+### Gerente de Marketing
+- Responsable de la planificación y ejecución de campañas de marketing para promocionar la marca de la empresa y aumentar las ventas.
+
+
---
+++ JAVA-XML-XSD/LecturaDelXmL.java
@@ -0,0 +1,101 @@
+//package com.mkyong.xml.dom;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.swing.*;
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.IOException;
+
+
+public class visualizar   {
+    visualizar(){
+        xml();
+    }
+
+
+
+    static void xml() {
+        //Path del Xml que se desea leer
+        final String FILENAME = "C://Users//westw//Desktop//planificacion-V1//Reto.xml";
+
+
+
+
+        // Instantiate the Factory
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+
+        try {
+
+            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+
+            // parse XML file
+            DocumentBuilder db = dbf.newDocumentBuilder();
+
+            Document doc = db.parse(new File(FILENAME));
+
+
+
+            doc.getDocumentElement().normalize();
+
+            System.out.println("Root Element :" + doc.getDocumentElement().getNodeName());
+            System.out.println("------");
+
+            // get <staff>
+            NodeList list = doc.getElementsByTagName("coche");
+
+            for (int temp = 0; temp < list.getLength(); temp++) {
+
+                Node node = list.item(temp);
+
+                if (node.getNodeType() == Node.ELEMENT_NODE) {
+
+                    Element element = (Element) node;
+
+                    //Obtener los elementos
+                    String id_coche = element.getElementsByTagName("id_coche").item(0).getTextContent();
+                    String matricula = element.getElementsByTagName("matricula").item(0).getTextContent();
+                    String marca = element.getElementsByTagName("marca").item(0).getTextContent();
+                    String modelo = element.getElementsByTagName("modelo").item(0).getTextContent();
+                    String caballos = element.getElementsByTagName("caballos").item(0).getTextContent();
+                    String color = element.getElementsByTagName("color").item(0).getTextContent();
+                    String ano = element.getElementsByTagName("ano").item(0).getTextContent();
+                    String consumo = element.getElementsByTagName("consumo").item(0).getTextContent();
+                    String precio_base = element.getElementsByTagName("precio_base").item(0).getTextContent();
+
+
+
+                    //Visualizar lso elementos
+                    System.out.println("id coche  : " + id_coche);
+                    System.out.println("matricula  : " + matricula);
+                    System.out.println("marca  : " + marca);
+                    System.out.println("modelo  : " + modelo);
+                    System.out.println("caballos  : " + caballos);
+                    System.out.println("color  : " + color);
+                    System.out.println("ano  : " + ano);
+                    System.out.println("consumo  : " + consumo);
+                    System.out.println("precio base  : " + precio_base);
+
+
+
+
+                }
+            }
+
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        new visualizar();
+    }
+
+
+}
